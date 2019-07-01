d5af846f106833dcb1ef1318671f506dbc24851a
nao20010128nao <nao20010128@gmail.com>
2019-07-01 02:47:23 +0000

    RKを修正

--- methods/rk_2.js
+++ methods/rk_2.js
@@ -19,13 +19,13 @@ module.exports = function*(func, x, y, t, h, len) {
       nextT = t.plus(h);
     // k[1234]はx、m[1234]はyに対応する
     const k1 = h.times(func(x, y, t));
-    const m1 = h.times(x);
-    const k2 = h.times(func(x.plus(k1.div(2)), y.plus(m1.div(2)), furtherT));
-    const m2 = h.times(k1.div(2).plus(x));
-    const k3 = h.times(func(x.plus(k2.div(2)), y.plus(m2.div(2)), furtherT));
-    const m3 = h.times(k2.div(2).plus(x));
-    const k4 = h.times(func(x.plus(k3), y.plus(m3), nextT));
-    const m4 = h.times(k3.plus(x));
+    const m1 = h.times(y);
+    const k2 = h.times(func(x.plus(m1.div(2)), y.plus(k1.div(2)), furtherT));
+    const m2 = h.times(m1.div(2).plus(y));
+    const k3 = h.times(func(x.plus(m2.div(2)), y.plus(k2.div(2)), furtherT));
+    const m3 = h.times(m2.div(2).plus(y));
+    const k4 = h.times(func(x.plus(m3), y.plus(k3), nextT));
+    const m4 = h.times(m3.plus(y));
     x = oldX.plus(BN.sum(k1, k2.times(2), k3.times(2), k4).div(6));
     y = oldY.plus(BN.sum(m1, m2.times(2), m3.times(2), m4).div(6));
 
