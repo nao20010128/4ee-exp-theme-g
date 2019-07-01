9301ea4e1f131bc7575b23c1acd13abd4b3c33ea
nao20010128nao <nao20010128@gmail.com>
2019-07-01 09:50:38 +0000

    インパルス応答の他にステップ応答も追加
    考察3はいらなかったと思う

---
+++ jobs/extra1_step.js
@@ -0,0 +1,30 @@
+const BN = require("bignumber.js");
+const euler = require("../methods/euler_2");
+const sols = require("../functions/secondorderlag");
+const step = require("../functions/step");
+
+// 考察1
+// このソースコードはオイラー法のものである。
+// また、ステップ応答である。
+
+// ξの指示が無いため、ξ=1とする。
+// また、ωの値はomgの配列の内容とした。
+const omg = ["0.5", "1", "1.5"].map(a => new BN(a));
+
+function* merger() {
+  // 関数のxの値を連結するため
+  const lines = omg.map(num => euler(sols(1, num, step, 1), 0, 0, 0, new BN("0.1"), 20));
+  while (true) {
+    const next = lines.map(a => a.next());
+    if (next[0].done) {
+      return;
+    }
+    const t = next[0].value[0];
+    const allX = next.map(a => a.value[1]);
+    yield [t, ...allX];
+  }
+}
+
+for (let line of merger()) {
+  console.log(line.map(a => +a).join(" "));
+}
