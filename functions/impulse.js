5559c02f43d1b8fc59ef9fd432dd9235fcef14ab
nao20010128nao <nao20010128@gmail.com>
2019-07-03 05:36:13 +0000

    時間ずらす

--- functions/impulse.js
+++ functions/impulse.js
@@ -8,7 +8,7 @@ const pulseHeight = 1 / pulseTime;
 
 module.exports = function(t) {
   t = new BN(t);
-  if (t.isZero()) {
+  if (t.eq(pulseTime)) {
     return pulseHeight;
   } else {
     return 0;
