ed58117e808b7c0bb6969d013a2fefa6ead2f787
nao20010128nao <nao20010128@gmail.com>
2019-07-03 00:18:07 +0000

    追加

--- jobs/extra5.js
+++ jobs/extra5.js
@@ -1,20 +1,18 @@
 const BN = require("bignumber.js");
-const euler = require("../methods/euler_2");
 const rk = require("../methods/rk_2");
 const sols = require("../functions/secondorderlag");
-const step = require("../functions/step");
+const sine = require("../functions/sine");
 
-// 考察3
-// このソースコードはオイラー法とルンゲ・クッタ法の出力を比較するものである。
-// また、ステップ応答である。
+// 考察5
+// このソースコードは時間幅を変えたことにより起こる誤差を見るものである。
+// ルンゲ・クッタ法を用いている。
 
-// ξの指示が無いため、配列zetaの内容とする。
-const zeta = ["0.6", 1, "2"];
+// ξの指示が無いため、ξ=0.6とする。
 // ωの指示が無いため、ω=1とする。
 
 function* merger() {
   // 関数のxの値を連結するため
-  const lines = zeta.map(num => [euler(sols(num, 1, step, 1), 0, 0, 0, new BN("0.1"), 20), rk(sols(num, 1, step, 1), 0, 0, 0, new BN("0.1"), 20)]).flat();
+  const lines = [rk(sols("0.6", 1, sine, 1), 0, 0, 0, new BN("0.1"), 30), rk(sols("0.6", 1, sine, 1), 0, 0, 0, new BN("0.01"), 30)];
   while (true) {
     const next = lines.map(a => a.next());
     if (next[0].done) {
