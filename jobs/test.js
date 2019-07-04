17229e7ca6b984bb0a035a1751e408fb75c76776
nao20010128nao <nao20010128@gmail.com>
2019-07-04 04:03:58 +0000

    時間と間隔をサンプルに合うように修正

--- jobs/test.js
+++ jobs/test.js
@@ -3,6 +3,6 @@ const test = require("../functions/test");
 
 // 予備実験
 
-for (let line of euler(test, 0, 0, "0.0001", 25)) {
+for (let line of euler(test, 0, 0, "0.01", 4 * Math.PI)) {
   console.log(line.map(a => +a).join(" "));
 }
